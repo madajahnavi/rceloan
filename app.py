838b7686b0a2848806646586b191584e9aae4990
madajahnavi <madajahnaviluckyrpj@gmail.com>
2025-03-27 15:32:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 import joblib
-clf = joblib.load("joblib(1).csv")
+clf = joblib.load("loan.joblib")
 st.title('LOAN APP RCE')
 g=st.number_input('Enter Gender 0:Male 1:Female')
 m=st.number_input('Enter Marital status 0:No 1:Yes')
